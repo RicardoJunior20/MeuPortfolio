7b66b62fadf9b2bd2c5825627f89f50952c84e8b
Ricardo Junior <ricardo.marley18@gmail.com>
2024-05-26 00:09:48 -0300

    Added muita coisa

--- scripts.js
+++ scripts.js
@@ -4,21 +4,30 @@ const contactForm = document.querySelector('#contact-form');
 contactForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    // Selecionar os campos do formulário
     const name = document.querySelector('#name').value;
     const email = document.querySelector('#email').value;
     const message = document.querySelector('#message').value;
 
-    // Aqui você pode adicionar código para enviar os dados do formulário para o servidor
+    // Validar se todos os campos foram preenchidos
+    if (name === '' || email === '' || message === '') {
+        // Se algum campo estiver vazio, exibir mensagem de erro
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Por favor, preencha todos os campos do formulário!',
+        });
+    } else {
+        // Se todos os campos estiverem preenchidos, exibir mensagem de sucesso
+        Swal.fire({
+            icon: 'success',
+            title: 'Mensagem Enviada!',
+            text: 'Obrigado pelo seu contato. Entrarei em contato em breve.',
+            showConfirmButton: false,
+            timer: 1500
+        });
 
-    // Exibe uma mensagem de sucesso (usando SweetAlert2)
-    Swal.fire({
-        icon: 'success',
-        title: 'Mensagem Enviada!',
-        text: 'Obrigado pelo seu contato. Entraremos em contato em breve.',
-        showConfirmButton: false,
-        timer: 1500
-    });
-
-    // Limpa o formulário
-    contactForm.reset();
+        // Limpar os campos do formulário após o envio
+        contactForm.reset();
+    }
 });
