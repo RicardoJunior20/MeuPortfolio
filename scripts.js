4f5888d62e5ddfca02ca59209008e2401cdad1f5
Ricardo Junior <ricardo.marley18@gmail.com>
2024-07-08 09:31:31 -0300

    ExclusaoDoCurriculo

--- scripts.js
+++ scripts.js
@@ -1,33 +1,37 @@
-// Seleção do formulário de contato
-const contactForm = document.querySelector('#contact-form');
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
 
-contactForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
 
-    // Selecionar os campos do formulário
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const message = document.querySelector('#message').value;
+    // Código de validação e envio do formulário de contato
+    const contactForm = document.querySelector('#contact-form');
 
-    // Validar se todos os campos foram preenchidos
-    if (name === '' || email === '' || message === '') {
-        // Se algum campo estiver vazio, exibir mensagem de erro
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por favor, preencha todos os campos do formulário!',
-        });
-    } else {
-        // Se todos os campos estiverem preenchidos, exibir mensagem de sucesso
-        Swal.fire({
-            icon: 'success',
-            title: 'Mensagem Enviada!',
-            text: 'Obrigado pelo seu contato. Entrarei em contato em breve.',
-            showConfirmButton: false,
-            timer: 1500
-        });
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-        // Limpar os campos do formulário após o envio
-        contactForm.reset();
-    }
+        const name = document.querySelector('#name').value;
+        const email = document.querySelector('#email').value;
+        const message = document.querySelector('#message').value;
+
+        if (name === '' || email === '' || message === '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Por favor, preencha todos os campos do formulário!',
+            });
+        } else {
+            Swal.fire({
+                icon: 'success',
+                title: 'Mensagem Enviada!',
+                text: 'Obrigado pelo seu contato. Entrarei em contato em breve.',
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+            contactForm.reset();
+        }
+    });
 });
